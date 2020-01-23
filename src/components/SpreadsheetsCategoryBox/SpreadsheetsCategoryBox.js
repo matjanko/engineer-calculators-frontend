45f7aa164f6ee48c5286c6383f57416ae50ee7f2
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-23 23:43:10 +0100

    Refactor 'SpreadsheetList' and 'SpreadsheetCategoryBox' components

--- src/components/SpreadsheetsCategoryBox/SpreadsheetsCategoryBox.js
+++ src/components/SpreadsheetsCategoryBox/SpreadsheetsCategoryBox.js
@@ -1,25 +1,16 @@
 import React from 'react'
 import './SpreadsheetsCategoryBox.css'
 import { Grid } from '@material-ui/core'
-import SpreadsheetTile from "../SpreadsheetTile";
 import TitleLabel from "../TitleLabel";
 
-export default function SpreadsheetsCategoryBox({name, spreadsheets}) {
+export default function SpreadsheetsCategoryBox({name, children}) {
 
     return (
         <div>
             <div className="root">
                 <TitleLabel title={name} isUnderlined={true}/>
                 <Grid container spacing={2}>
-                    {spreadsheets.map(spreadsheet => (
-                        <Grid item xs={12} sm={6} md={4} xl={4} key={spreadsheet.name}>
-                            <SpreadsheetTile
-                                name={spreadsheet.name}
-                                path={spreadsheet.path}
-                                description={spreadsheet.description}
-                                standard={spreadsheet.standard}/>
-                        </Grid>
-                    ))}
+                    {children}
                 </Grid>
             </div>
         </div>
--- src/components/SpreadsheetsList/SpreadsheetsList.js
+++ src/components/SpreadsheetsList/SpreadsheetsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './SpreadsheetsList.css'
 import { Grid } from '@material-ui/core'
 import SpreadsheetsCategoryBox from '../SpreadsheetsCategoryBox'
+import SpreadsheetTile from "../SpreadsheetTile/SpreadsheetTile";
 
 export default function SpreadsheetsList({spreadsheets}) {
 
@@ -10,7 +11,17 @@ export default function SpreadsheetsList({spreadsheets}) {
             <Grid container spacing={5}>
                 {spreadsheets.map(spreadsheet => (
                     <Grid item xs={12} key={spreadsheet.name}>
-                        <SpreadsheetsCategoryBox name={spreadsheet.name} spreadsheets={spreadsheet.sheets}/>
+                        <SpreadsheetsCategoryBox name={spreadsheet.name}>
+                            {spreadsheet.sheets.map(sheet => (
+                                <Grid item xs={12} sm={6} md={4} xl={4} key={sheet.name}>
+                                <SpreadsheetTile
+                                    name={sheet.name}
+                                    path={sheet.path}
+                                    description={sheet.description}
+                                    standard={sheet.standard}/>
+                                </Grid>
+                            ))}
+                        </SpreadsheetsCategoryBox>
                     </Grid>
                 ))}
             </Grid>
