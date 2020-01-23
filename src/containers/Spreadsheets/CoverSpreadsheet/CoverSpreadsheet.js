a239b269a9cd55853e0c19fe43af87c04e1c76c5
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-24 00:05:54 +0100

    Insert 'GroupBox' into 'CoverSpreadsheet'

--- src/containers/Spreadsheets/CoverSpreadsheet/CoverSpreadsheet.js
+++ src/containers/Spreadsheets/CoverSpreadsheet/CoverSpreadsheet.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Spreadsheet from "../../../components/Spreadsheet/Spreadsheet";
+import Grid from "@material-ui/core/Grid";
+import GroupBox from "../../../components/GroupBox";
 
 export default function CoverSpreadsheet() {
 
     return (
         <Spreadsheet title={'Otulina wg PN-EN-1992-1-1'}>
+            <Grid container>
+                <Grid item xs={12} md={6} lg={6}>
+                    <GroupBox header="Materiał">
 
+                    </GroupBox>
+                    <GroupBox header="Materiał">
+
+                    </GroupBox>
+                </Grid>
+                <Grid item xs={12} md={6} lg={6}>
+
+                    <GroupBox header="Materiał">
+
+                    </GroupBox>
+                    <GroupBox header="Materiał">
+
+                    </GroupBox>
+                    <GroupBox header="Materiał">
+
+                    </GroupBox>
+                </Grid>
+            </Grid>
         </Spreadsheet>
     )
 }
