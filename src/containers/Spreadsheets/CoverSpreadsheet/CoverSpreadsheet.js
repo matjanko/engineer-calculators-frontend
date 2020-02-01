af74c2e87be31d78155b4d793502f0ccd2674e61
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-01 22:02:01 +0100

    Implemented fetching concretes in 'CoverSpreadsheet' component using React useEffect hook

--- src/containers/Spreadsheets/CoverSpreadsheet/CoverSpreadsheet.js
+++ src/containers/Spreadsheets/CoverSpreadsheet/CoverSpreadsheet.js
@@ -1,34 +1,46 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Spreadsheet from "../../../components/Spreadsheet/Spreadsheet";
 import Grid from "@material-ui/core/Grid";
-import GroupBox from "../../../components/GroupBox";
+import MaterialGroupBox from "./MaterialGroupBox";
 
-export default function CoverSpreadsheet() {
+
+const CoverSpreadsheet = () => {
+
+    const [hasError, setErrors] = React.useState(false);
+    const [concretes, setConcretes] = React.useState([]);
+
+    const [selectedConcrete, setSelectedConcrete] = React.useState('');
+
+    async function fetchConcretes() {
+        const response = await fetch('/api/concretes/classes');
+        return await response.json()
+    }
+
+    useEffect( () => {
+        fetchConcretes()
+            .then(setConcretes)
+            .catch(err => setErrors(err));
+
+    }, []);
+
+    const handleConcreteOnChange = event => {
+        setSelectedConcrete(event.target.value)
+    };
 
     return (
         <Spreadsheet title={'Otulina wg PN-EN-1992-1-1'}>
             <Grid container>
                 <Grid item xs={12} md={6} lg={6}>
-                    <GroupBox header="Materiał">
-
-                    </GroupBox>
-                    <GroupBox header="Materiał">
-
-                    </GroupBox>
+                    <MaterialGroupBox concretes={concretes}
+                                      concrete={selectedConcrete}
+                                      concreteOnChange={handleConcreteOnChange}/>
                 </Grid>
                 <Grid item xs={12} md={6} lg={6}>
 
-                    <GroupBox header="Materiał">
-
-                    </GroupBox>
-                    <GroupBox header="Materiał">
-
-                    </GroupBox>
-                    <GroupBox header="Materiał">
-
-                    </GroupBox>
                 </Grid>
             </Grid>
         </Spreadsheet>
     )
-}
+};
+
+export default CoverSpreadsheet;
