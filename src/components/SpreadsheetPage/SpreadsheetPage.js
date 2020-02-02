5b5d4865f3f0cd1694de0cee172be1da0fcb9318
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-02 14:18:07 +0100

    Initial 'SpreadsheetError' component into 'SpreadsheetPage'

--- src/components/SpreadsheetPage/SpreadsheetPage.js
+++ src/components/SpreadsheetPage/SpreadsheetPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import TitleLabel from "../TitleLabel";
+import SpreadSheetError from "../SpreadsheetError";
 
 const SpreadsheetPage = ({title = '', children, hasError = true}) => {
+    const errorText = 'Wystąpił nieoczekiwany błąd serwera. Kalkulator jest niedostępny';
+
     return (
         <div>
             {title.length > 0 ? <TitleLabel title={title} isUnderlined={true}/> : ''}
-            {hasError ? "błąd" : children}
+            {hasError ? <SpreadSheetError errorText={errorText}/> : children}
         </div>
     )
 };
