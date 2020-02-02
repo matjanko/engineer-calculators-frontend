f447c84f4ea7ebbeff94f2fe4247dc208e6d5b87
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-02 16:14:28 +0100

    SpreadsheetLabel component

---
+++ src/components/SpreadsheetLabel/SpreadsheetLabel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SpreadsheetLabel = ({text}) => {
+    return (
+        <label className="spreadsheet-label-root">
+            {text}
+        </label>
+    )
+};
+
+export default SpreadsheetLabel
